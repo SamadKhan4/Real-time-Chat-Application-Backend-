027d419104662512f0b1a72a9ae1f6a3b8062838
Sam Khan <samadkhan040204@gmail.com>
2026-05-16 12:00:20 +0530

    all set

--- server.js
+++ server.js
@@ -31,6 +31,20 @@ io.on("connection" ,(socket) =>{
     //Emit online user to all conected client
     io.emit("getOnlineUsers" , Object.keys(userSocketMap));
 
+    socket.on("typing", ({ receiverId }) => {
+        const receiverSocketId = userSocketMap[receiverId];
+        if(receiverSocketId) {
+            io.to(receiverSocketId).emit("typing", { senderId: userId });
+        }
+    })
+
+    socket.on("stopTyping", ({ receiverId }) => {
+        const receiverSocketId = userSocketMap[receiverId];
+        if(receiverSocketId) {
+            io.to(receiverSocketId).emit("stopTyping", { senderId: userId });
+        }
+    })
+
     socket.on("disconnect" , () =>{
         console.log("user disconnected" , userId);
         delete userSocketMap[userId];
